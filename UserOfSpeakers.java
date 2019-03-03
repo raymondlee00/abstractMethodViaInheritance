34cf09270368f2a184fb246db731de9c97b5cb50
Raymond <rlee00@stuy.edu>
2019-03-03 02:30:55 -0500

    modification

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -2,6 +2,6 @@ public class UserOfSpeakers {
   public static void main(String[] args) {
     retriever1 = new Retriever();
     Animal animal = retriever1;
-    System.out.println(Animal.speak());
+    System.out.println(animal.speak());
   }
 }
