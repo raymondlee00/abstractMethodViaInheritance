5e6fd0f1c3b1b142e35dfd9d506a9ec8bb7546c3
Raymond <rlee00@stuy.edu>
2019-03-03 02:31:52 -0500

    new commit

---
+++ abstractMethodViaInheritance/Retriever.java
@@ -0,0 +1,6 @@
+public class Retriever extends Pet{
+    public String speak() {
+        return "arf";
+    }
+}
+//hello jefe el
---
+++ abstractMethodViaInheritance/UserOfSpeakers.java
@@ -0,0 +1,6 @@
+public class UserOfSpeakers {
+	public static void main(String[] args) {
+		Animal ret = new Retriever();
+		System.out.println(ret.speak());
+	}
+}
