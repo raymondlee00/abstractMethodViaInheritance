ab29fbb9537452f519cf513df7dd80739bc4cd8d
unknown <stan01@stuy.edu>
2019-03-06 21:44:47 -0500

    modified by sam

--- abstractMethodViaInheritance/UserOfSpeakers.java
+++ abstractMethodViaInheritance/UserOfSpeakers.java
@@ -1,4 +1,4 @@
-public class UserOfSpeakers {
+public class UserOfSpeakers { //modified by samuel
 	public static void main(String[] args) {
 		Animal ret = new Retriever();
 		System.out.println(ret.speak());
